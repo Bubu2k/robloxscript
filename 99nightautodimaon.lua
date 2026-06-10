25b8f6ddf644ade1d41aad59820b8c77d4961708
GIA BAO <giabaohuynh041@gmail.com>
2026-06-10 15:48:58 +0700

    Create 99nightautodimaon.lua

---
+++ 99nightautodimaon.lua
@@ -0,0 +1,4 @@
+
+getgenv().maxday = 100
+getgenv().webhook = "Url Here" -- sends after max day reached
+loadstring(game:HttpGet('https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/FNDayFarm.lua'))()
